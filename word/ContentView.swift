83780ded35151bf29504f7df1e26de4a1a7a9699
Eatmyself <nagin2358@gmail.com>
2021-04-02 21:24:22 +0800

    Initial Commit

---
+++ word/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  word
+//
+//  Created by Eatmyself on 2021/4/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ word/wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordApp.swift
+//  word
+//
+//  Created by Eatmyself on 2021/4/2.
+//
+
+import SwiftUI
+
+@main
+struct wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
